119d3c4aed6dccf29a1b77c430e7d205b6f36923
Maddy <38600883+Maddy6565@users.noreply.github.com>
2026-07-15 11:04:31 -0400

    Terraform (#10)
    * modules ec2
    
    * modules-vpc

---
+++ modules/ec2/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+    type =  string
+    default = "ami-0e5497a77ef21b5ac"
+    description =  "This is the Ubuntu server as default"
+}
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+    description = "The is default size"
+}
