9fa13a1a805bd67c2689b1abee644213fe1d6a4d
Maddy6565 <manideep.puligilla1998@gmail.com>
2026-07-21 10:28:04 -0400

    main

--- mainvpc.tf
+++ mainvpc.tf
@@ -2,18 +2,35 @@ module "vpc" {
 
   source = "./modules/vpc"
 
-  name = "terraform-demo-vpc-2"
+  name = "Demo-vpc-1"
 
   vpc_cidr = "10.0.0.0/24"
 
 }
+
+module "subnet" {
+
+    source = "./modules/subnet"
+
+    vpc_id = module.vpc.vpc_id
+
+    availability_zone = "us-eat-2"
+
+    subnet_cidr = 10.0.3.0/24
+
+    subnet_tags = {
+      Name = "Demo-security-group"
+    }
+
+}
+
 module "ec2" {
 
     source = "./modules/ec2"
 
     ec2_tags = {
 
-        Name = "Demo-testing-2"
+        Name = "Demo-testing-1"
     }
 
 }
