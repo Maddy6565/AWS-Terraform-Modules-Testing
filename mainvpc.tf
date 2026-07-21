1c7fe483dcf68e817423fece2794a533a798c10a
Maddy6565 <manideep.puligilla1998@gmail.com>
2026-07-21 14:27:57 -0400

    main

--- mainvpc.tf
+++ mainvpc.tf
@@ -24,6 +24,22 @@ module "subnet" {
 
 }
 
+module "subnet" {
+
+  source = "./modules/subnet"
+
+  vpc_id = module.vpc.vpc_id
+
+  availability_zone = "us-east-2b"
+
+  cidr_block = "10.0.1.0/24"
+
+  subnet_tags = {
+    Name = "Demo-subnet-2"
+  }
+
+}
+
 module "igw" {
 
   source = "./modules/igw"
@@ -49,6 +65,21 @@ module "ec2" {
   ec2_tags = {
 
     Name = "Demo-testing-1"
+  }
+
+}
+module "ec2" {
+  
+  source = "./modules/ec2"
+
+  instance_type = "t3.micro"
+
+  ami_id = "ami-0daff962b1c050d36"
+
+  ec2_tags = {
+
+    Name = "Demo-testing-ec2"
+
   }
 
 }
