8c4387c43a26d04c0796ef7a1247dbaca0eb8b9d
Maddy6565 <manideep.puligilla1998@gmail.com>
2026-08-05 10:44:20 -0400

    s3 bucket

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -14,6 +14,16 @@ resource "aws_security_group" "dev_sg" {
 
   }
 
+  ingress {
+
+    description = "All Traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
   egress {
 
     from_port   = 0
