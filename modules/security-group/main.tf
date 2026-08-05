ef0309eb5a3102ce085d60fa016e70f12470e275
Maddy6565 <manideep.puligilla1998@gmail.com>
2026-08-05 10:10:44 -0400

    changes in sg

--- modules/security-group/main.tf
+++ modules/security-group/main.tf
@@ -6,10 +6,10 @@ resource "aws_security_group" "dev_sg" {
 
   ingress {
 
-    description = "All Traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
 
   }
